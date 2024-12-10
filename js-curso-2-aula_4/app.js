d2a63c316916c19794b1cdb90a716eb4b92c8ac4
Gabriel Carvalho <gabigol3212017@outlook.com>
2024-12-10 11:40:02 -0300

    mudei letra i linha 23

--- js-curso-2-aula_4/app.js
+++ js-curso-2-aula_4/app.js
@@ -20,7 +20,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Isso aí, você acertou');
+        exibirTextoNaTela('h1', 'Isso ai, você acertou');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
